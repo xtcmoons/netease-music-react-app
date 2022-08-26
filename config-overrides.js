380454a31d7b70b37ace5a4dcae3f2c506e30a6f
XTCm <xtcmoons@yeah.net>
2022-08-26 15:50:59 +0800

    add home

--- config-overrides.js
+++ config-overrides.js
@@ -9,9 +9,6 @@ module.exports = {
       lessLoaderOptions: {
         lessOptions: {},
       },
-    }),
-    addWebpackAlias({
-      '@': path.resolve(__dirname, 'src'),
     })
   ),
 };
---
+++ src/hooks/redux.ts
@@ -0,0 +1,1 @@
+import { TypedUseSelectorHook } from 'react-redux';
