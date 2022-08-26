4b27fef6889606222a7f89c43d35dc45250cdba8
XTCm <xtcmoons@yeah.net>
2022-08-26 15:05:49 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log('---> ');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header1">
